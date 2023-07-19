4bb61b78634d97027e24f463941fca141511f25f
Mahsa Arjmand <103898788+mahsaarj@users.noreply.github.com>
2023-07-19 07:26:15 -0700

    changing nodes colors / not working properly

--- RectangleShape.java
+++ RectangleShape.java
@@ -2,16 +2,12 @@ package Package1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RectangleShape extends JPanel {
     private Color color;
     private int size;
-
-    public RectangleShape(Color color, int size) {
-        this.color = color;
-        this.size = size;
-        setPreferredSize(new Dimension(size, size));
-    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -20,19 +16,36 @@ public class RectangleShape extends JPanel {
         g.fillRect(0, 0, getWidth(), getHeight());
     }
 
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
+    public RectangleShape(Color color, int size) {
         this.color = color;
-        repaint();
-    }
+        this.size = size;
+        setPreferredSize(new Dimension(size, size));
 
-    //@Override
-    //public void Paint(Color color) {
-      //  this.color = color;
-        //repaint();
-    //}
+        // Add mouse listeners
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // Color the node that the rectangle enters with green
+                setBackground(Color.PINK);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // Color the node that the rectangle exits with red
+                setBackground(Color.RED);
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                // Get the node that the rectangle is currently over
+                JPanel node = (JPanel) e.getComponent().getParent();
+
+                // Set the color of the node to green
+                node.setBackground(Color.PINK);
+            }
+        });
+    }
 
 }
